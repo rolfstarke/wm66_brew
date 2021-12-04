d59c8fcbdf732cc669aadc3df715753059409676
rolfstarke <r-level2@posteo.de>
2021-12-04 20:14:08 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   gpio_test.py

---
+++ gpio_test.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.output(11, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
+
+time.sleep(10)
+
+GPIO.output(11, GPIO.HIGH)
+GPIO.output(13, GPIO.HIGH)
