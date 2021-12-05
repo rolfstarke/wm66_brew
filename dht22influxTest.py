cc3ad594882fe9bff0416d855140b819f67a9f95
Karl-Peter Leiß <karl-peter.leiss@posteo.de>
2021-12-05 20:06:22 +0100

    bugfixes

--- dht22influxTest.py
+++ dht22influxTest.py
@@ -1,29 +1,30 @@
 import sys
-import adafruit_dht
+import Adafruit_DHT
 import time
 from influxdb import client as influxdb
 import datetime
+from pytz import timezone
 
-humidity, temperature = adafruit_dht.read(22, 4)
+humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT22, 4)
 
 if humidity is not None and temperature is not None:
-	influxHost = 'localhost'
-	influxPort = '8086'
-	influxUser = 'admin'
-	influxPasswd = '12TUDdd.47'
-	influxdbName = 'dhtTest'
+        influxHost = 'localhost'
+        influxPort = '8086'
+        influxUser = 'grafana'
+        influxPasswd = '12TUDdd.47'
+        influxdbName = 'dhtTest'
 
-	influx_metric = [{
-		'measurement': 'TemperatureSensor',
-		'time': datetime.datetime.now(),
-		'fields': {
-			'temperature': temperature,
-			'humidity': humidity
-		}
-	}]
+        influx_metric = [{
+                'measurement': 'TemperatureSensor',
+                'time': datetime.datetime.now(timezone('CET')),
+                'fields': {
+                        'temperature': temperature,
+                        'humidity': humidity
+                }
+        }]
 
-	try:
-		db = influxdb.InfluxDBClient(influxHost, influxPort, influxUser, influxPasswd, influxdbName)
-		db.write_points(influx_metric)
-	finally:
-		db.close()
+        try:
+                db = influxdb.InfluxDBClient(influxHost, influxPort, influxUser, influxPasswd, influxdbName)
+                db.write_points(influx_metric)
+        finally:
+                db.close()
