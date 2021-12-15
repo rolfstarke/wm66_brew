2e523ae63c8ae324635ce550766daf41c9b82518
Karl <92539870+kpleiss@users.noreply.github.com>
2021-12-15 16:00:48 +0100

    cosmetics

--- brew.py
+++ brew.py
@@ -71,12 +71,12 @@ def heater_control(target_temp):
 	if current_temp() < target_temp :
 		GPIO.output(heater_pin, GPIO.LOW)
 		time.sleep(relay_interval)
-		print(str(datetime.datetime.now(timezone('CET'))) + " | current temperature: " + str(round(current_temp(), 1)) + " °C " + "| heater: on    ", end='\r')
+		print("["str(datetime.datetime.now(timezone('CET')))"]" + " | current temperature: " + str(round(current_temp(), 1)) + " °C " + "| heater: on    ", end='\r')
 		writeInflux(current_temp(), target_temp)
 	else:
 		GPIO.output(heater_pin, GPIO.HIGH)
 		time.sleep(relay_interval)
-		print(str(datetime.datetime.now(timezone('CET'))) + " | current temperature: " + str(round(current_temp(), 1)) + " °C " + "| heater: idle    ", end='\r')
+		print(str("["datetime.datetime.now(timezone('CET')))"]" + " | current temperature: " + str(round(current_temp(), 1)) + " °C " + "| heater: idle    ", end='\r')
 		writeInflux(current_temp(), target_temp)
 
 # durchgehen der Rasten
