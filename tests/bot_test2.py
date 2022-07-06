ab37b3f24ba700119f1aeee32ddc7b9620c26c8d
Karl Leiß <karl-peter.leiss@posteo.de>
2022-07-06 18:51:51 +0200

    modified:   bot_send_msg.py 	modified:   tests/bot_test2.py

--- tests/bot_test2.py
+++ tests/bot_test2.py
@@ -3,7 +3,7 @@ import json
 
 def sendMsg(msg):
     #Pfad zum Token einfuegen    
-    with open("token.txt") as file:
+    with open("../data/token.txt") as file:
         token = file.read().splitlines()
     url = f"https://api.telegram.org/bot{token[0]}/sendMessage"
     params = {"chat_id":"-792733418", "text":msg}
