b49d1db83c279434c3f1aa9156704de15b444a62
slorward <rolf>
2023-02-14 21:10:59 +0100

    debugging

--- brew_package/cool.py
+++ brew_package/cool.py
@@ -11,5 +11,7 @@ def cool(relay_interval, measurement_name):
     while current_temp() > cooling_temp:
         time.sleep(relay_interval)
         writeInflux(current_temp(), cooling_temp, measurement_name)
+        print("[" + str(datetime.datetime.now(timezone('CET'))) + "]" + " | current temperature: " +
+              str(round(current_temp(), 1)) + " °C ", end='\r')
     print("target temperature reached")
     sendMsg("Wuerze auf " + str(cooling_temp) + " Grad gekuehlt. Schmecken lassen!")
