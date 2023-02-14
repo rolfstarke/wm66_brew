5d92f057c1f1d622d3efb2d496d679f50d793ac9
slorward <rolf>
2023-02-14 20:28:52 +0100

    debugging

--- brew_package/agitate.py
+++ brew_package/agitate.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO
 import time
 
 def agitate(agitator_pin):
-    agitation_time = int(input("minutes to agitate for? ") * 60)
-    print(agitation_time)
+    agitation_time = int(input("minutes to agitate for? "))
+    print(agitation_time * 60)
     GPIO.output(agitator_pin, GPIO.HIGH)
-    time.sleep(agitation_time)
+    time.sleep(agitation_time * 60)
     GPIO.output(agitator_pin, GPIO.LOW)
     print("agitation finished")
