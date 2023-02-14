77c128ab0295159fb7416fb1a8935255feb52706
slorward <rolf>
2023-02-14 21:05:13 +0100

    debugging

--- brew_package/cool.py
+++ brew_package/cool.py
@@ -8,7 +8,7 @@ from utils import writeInflux
 def cool(relay_interval, measurement_name): 
     cooling_temp = float(input("deploy wort cooler and input target temperature "))
     sendMsg("Jetzt wird abgekuehlt!")
-    while current_temp() < cooling_temp:
+    while current_temp() > cooling_temp:
         time.sleep(relay_interval)
         writeInflux(current_temp(), cooling_temp, measurement_name)
     print("target temperature reached")
