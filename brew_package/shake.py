015cb3e462f858f0444e4eb2105de09614b90eba
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 20:37:47 +0200

    Update shake.py

--- brew_package/shake.py
+++ brew_package/shake.py
@@ -5,6 +5,6 @@ import time
 def shake(agitator_pin):
    
     GPIO.output(agitator_pin, GPIO.HIGH)
-    time.sleep(1)
+    time.sleep(0.3)
     GPIO.output(agitator_pin, GPIO.LOW)
     print("agitation finished")
