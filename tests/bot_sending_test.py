3c1803ffd366ec2a0d3ee7b1f38320ba00f35e5d
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 21:41:13 +0200

    Update bot_sending_test.py

--- tests/bot_sending_test.py
+++ tests/bot_sending_test.py
@@ -6,5 +6,5 @@ def sendMsg(msg):
     with open("../data/token.txt") as file:
         token = file.read().splitlines()
     url = f"https://api.telegram.org/bot{token[0]}/sendMessage"
-    params = {"chat_id":"-924663757", "text":msg}
+    params = {"chat_id":"-1971489754", "text":msg}
     message = requests.post(url, params=params)
