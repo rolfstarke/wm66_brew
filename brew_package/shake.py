80615ca94cc2a08149f2463e49e9563b42886e0c
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 22:22:00 +0200

    Update shake.py

--- brew_package/shake.py
+++ brew_package/shake.py
@@ -1,13 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
+# Konstanten für die Dauer der Agitation und die Verzögerung zwischen den Schüttelbewegungen
+AGITATOR_DURATION = 0.1
+DELAY_BETWEEN_SHAKES = 1
+
 def shake(agitator_pin):
-   counter = 0
-   shakes = int(input("Wie of wollt ihr Rütteln? Und wehe ihr sperrt mich wieder aus Chatgruppe aus. Bratzen. "))
-   while counter < int(shakes):
-      GPIO.output(agitator_pin, GPIO.HIGH)
-      time.sleep(0.05)
-      GPIO.output(agitator_pin, GPIO.LOW)
-      counter += 1
-      time.sleep(2)
-   print("Agitation finished")
+    try:
+        # GPIO-Modus auf BOARD setzen und den angegebenen Pin als Ausgang konfigurieren
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(agitator_pin, GPIO.OUT)
+        
+        # Benutzerabfrage für die Anzahl der Schüttelbewegungen
+        while True:
+            shakes_input = input("Wie oft soll die Agitation durchgeführt werden? ")
+            try:
+                shakes = int(shakes_input)
+                break
+            except ValueError:
+                print("Ungültige Eingabe. Bitte geben Sie eine ganze Zahl ein.")
+        
+        # Schleife für die angegebene Anzahl an Schüttelbewegungen durchlaufen
+        counter = 0
+        while counter < shakes:
+            GPIO.output(agitator_pin, GPIO.HIGH)
+            time.sleep(AGITATOR_DURATION)
+            GPIO.output(agitator_pin, GPIO.LOW)
+            counter += 1
+            time.sleep(DELAY_BETWEEN_SHAKES)
+            
+        print("Agitation beendet ;-)")
+    
+    finally:
+        # GPIO-Modus zurücksetzen und alle Pins freigeben
+        GPIO.cleanup()
