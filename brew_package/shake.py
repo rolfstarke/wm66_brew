93c874ed16e2d2e6c6b226e4312460571c2a7388
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 21:58:52 +0200

    Update shake.py

--- brew_package/shake.py
+++ brew_package/shake.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-def shake(agitator_pin, shakes):
+def shake(agitator_pin):
    counter = 0
+   shakes = int(input("Wie of wollt ihr Rütteln? Und wehe ihr sperrt mich wieder aus Chatgruppe aus. Bratzen. "))
    while counter < int(shakes):
       GPIO.output(agitator_pin, GPIO.HIGH)
       time.sleep(0.1)
