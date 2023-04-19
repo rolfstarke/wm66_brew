177334f28ba6d7d4a992ae6438eeb097376fba45
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 20:33:31 +0200

    Create shake.py

---
+++ brew_package/shake.py
@@ -0,0 +1,10 @@
+
+import RPi.GPIO as GPIO
+import time
+
+def shake(agitator_pin):
+   
+    GPIO.output(agitator_pin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(agitator_pin, GPIO.LOW)
+    print("agitation finished")
