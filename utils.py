5de32be2149e24c7dabd3f1b7f39c1e778f5ced7
Karl <92539870+kpleiss@users.noreply.github.com>
2023-04-19 21:13:41 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -65,5 +65,5 @@ def sendMsg(msg):
     with open("data/token.txt") as file:
         token = file.read().splitlines()
     url = f"https://api.telegram.org/bot{token[0]}/sendMessage"
-    params = {"chat_id":"-792733418", "text":msg}
+    params = {"chat_id":"-1971489754", "text":msg}
     message = requests.post(url, params=params)
